938f48d15f38b04c10f56adac89b9342855b0fb4
Aula72 <simaulat@gmail.com>
2022-11-04 18:56:49 +0300

    db3400

--- config/constants.php
+++ config/constants.php
@@ -1,15 +1,15 @@
 <?php
 //constant Variables
 
-define("DBHOST", "127.0.0.1");
-define("DBUSER", "root");
-define("DBPASS", "");
-define("DBNAME", "cmmf");
+// define("DBHOST", "127.0.0.1");
+// define("DBUSER", "root");
+// define("DBPASS", "");
+// define("DBNAME", "cmmf");
 
-// define("DBHOST_V2","localhost");
-// define("DBUSER_V2","keberaorganics_wolfarm");
-// define("DBPASS_V2","(wolfarm2424)");
-// define("DBNAME_V2","keberaorganics_kebera_v2");  
+define("DBHOST_V2","localhost");
+define("DBUSER_V2","twaha_adin");
+define("DBPASS_V2","B9?({a,^}*5v");
+define("DBNAME_V2","cmmf");  
 
 
 define("APP_NAME",'KeBERA');
