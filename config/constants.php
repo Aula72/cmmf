7becb3a53f4eaf67a2186ca210c5d072b65e9579
Aula72 <simaulat@gmail.com>
2022-11-12 16:52:45 +0300

    sjdksdjk

--- config/constants.php
+++ config/constants.php
@@ -1,15 +1,20 @@
 <?php
 //constant Variables
 
-// define("DBHOST", "127.0.0.1");
-// define("DBUSER", "root");
-// define("DBPASS", "");
-// define("DBNAME", "cmmf");
+if($_SERVER['HTTP_HOST']=='cmmf.fueless.co.ug'){
+    define("DBHOST","localhost");
+    define("DBUSER","twaha_adin");
+    define("DBPASS","B9?({a,^}*5v");
+    define("DBNAME","cmmf");  
+}else{
+    define("DBHOST", "127.0.0.1");
+    define("DBUSER", "root");
+    define("DBPASS", "");
+    define("DBNAME", "cmmf");
+}
 
-define("DBHOST","localhost");
-define("DBUSER","twaha_adin");
-define("DBPASS","B9?({a,^}*5v");
-define("DBNAME","cmmf");  
+
+
 
 
 define("APP_NAME",'KeBERA');
