f01a57bbbf1b5f6aafa52695c2ff513296785e2e
Aula72 <simaulat@gmail.com>
2023-02-07 18:17:13 +0300

    cvcvc

--- controllers/Helpers.php
+++ controllers/Helpers.php
@@ -421,7 +421,7 @@ class Helper{
     // }
 
     public function write_2_file($file, $txt){
-        // $file ="logs/".$file;
+        $file ="logs/".$file;
         $myfile = fopen($file, "a") or die("Unable to open file!");
         
         fwrite($myfile, date('d-m-Y H:i:s').">>>".$txt."\n");
